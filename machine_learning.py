0698e0f0a03a8a3a3ad5d9b3ffdf9ec05f6e1a26
liunan-quantactic <finaholic@163.com>
2022-04-27 19:56:46 +0800

    数据调整

--- machine_learning.py
+++ machine_learning.py
@@ -1,27 +1,36 @@
+# ML-sklearning 自带数据
+# 设置原始数据使用鸢尾花数据
+import pandas as pd
+from sklearn import datasets
+iris_attr = datasets.load_iris().data
+iris_class = datasets.load_iris().target
+
+
 '''感知机'''
 import matplotlib.pyplot as plt
 import numpy as np
 # 解决数据的二分问题的监督学习
-# 设置原始数据：假如有n个点
-fig = plt.figure()
-ax = fig.add_subplot()
 # 样本数据
-x1 = [0,0.2,0.5,0.6,0.8,0.8,1.0,1,0.1,0.2,0.3,0.4,0,0.5]
-x2 = [0,0.3,0.5,0.2,0.6,0.2,0.5,1,0.5,0.7,0.5,0.7,1,1]
-# 标签(label监督学习一定要)
-label = [0,0,0,0,0,0,0,0,1,1,1,1,1,1]
+X = pd.DataFrame(iris_attr[:,2:],columns=['A1','A2'])
+# 讲分类加入这个数据
+X.insert(0,'class',iris_class)
+# 由于是二分问题，所以之做2个种类的数据,由于是监督学习，因此必须要label
+df = X[X['class'] != 2]
 # 使用sklearn的包进行学习
 from sklearn.linear_model import Perceptron
-X_train = list(zip(x1,x2))
-preceptron=Perceptron(n_iter=1000)
-preceptron.fit(X_train,label)
+X_train = df[['A1','A2']]
+preceptron=Perceptron()
+preceptron.fit(X_train,df['class'])
 w=preceptron.coef_
 b=preceptron.intercept_
-print("感知器的分类准确度为：{:.2f}".format(preceptron.score(X_train,label)))
-plt.scatter(x1[0:8],x2[0:8], color="red",label="positive")
-plt.scatter(x1[8:],x2[8:], color="green",label="negative")
-line_x = np.arange(-0.1,1.5,0.1)
-line_y = line_x*(-w[0][0]-b[0])/w[0][1]
+print("感知器的分类准确度为：{:.2f}".format(preceptron.score(X_train,df['class'])))
+#把图画出来
+fig = plt.figure()
+ax = fig.add_subplot()
+plt.scatter(df[df['class']==0]['A1'],df[df['class']==0]['A2'], color="red",label="positive")
+plt.scatter(df[df['class']==1]['A1'],df[df['class']==1]['A2'], color="green",label="negative")
+line_x = np.arange(0,5,1)
+line_y = -(line_x*w[0][0]+b[0])/w[0][1]
 plt.plot(line_x,line_y)
 plt.legend(loc="best")
 plt.show()
