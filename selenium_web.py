26696c25b049a84ddccfc9ee7e312b9ef3db2537
liunan-quantactic <finaholic@163.com>
2022-04-29 10:41:57 +0800

    none

--- selenium_web.py
+++ selenium_web.py
@@ -2,10 +2,24 @@
 '''https://chromedriver.storage.googleapis.com/index.html'''
 #下载完成之后，解压，将其放置在Python安装路径下Scripts文件夹中即可
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 #测试能否打开浏览器
-browser=webdriver.Chrome()
+driver=webdriver.Chrome()
 #打开表示成功
 #打开目标网页
-url=r'https://www.21cake.com/'
-browser.get(url)
-browser.find_element(by="上海").click()
+url = r'https://www.baidu.com/'
+driver.get(url)
+driver.title
+'''可以查找的元素类型有8类
+NAME,ID,XPATH,CLASS_NAME,CSS_SELECTOR,LINK_TEXR,PARTIAL_LINK_TEXT,TAG_NAME'''
+search_box = driver.find_element(By.NAME, "wd")
+search_button = driver.find_element(By.ID, "su")
+'''对元素可以进行的操作
+填写 send_keys
+点击 .click()
+'''
+search_box.send_keys("Selenium")
+search_button.click()
+driver.find_element(By.NAME, "q").get_attribute("value") # => "Selenium"
+driver.quit()
+
